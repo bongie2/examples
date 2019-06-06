b16c26c86132b5016f42bd91b1ad22787d178ebc
Bongie <makgomo05@gamil.com>
2019-06-06 14:19:51 +0200

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fromStringWithSourceMap } from 'source-list-map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'examples';
+  title = 'Bongie';
+ 
+//Fruits=["Apple","Banana","Pear","pineApple"];
+//removeFruits(Fruit)
+//{
+  //let index= this.Fruits.indexOf(Fruit)
+  //this.Fruits.splice(index,1)
+  
+//}
+//lists=["Animals","Fruits","Vegetables"];
+//newlist:string='';
+//AddNew(){
+  
+  //this.lists.push(this.newlist)
+//}
+//RemoveList(list){
+  //let index=this.lists.indexOf(list)
+  //this.lists.splice(index,1)
+//}
+lists=["Animals","Fruits","Vegetables"];
+newlist:string='';
+AddNew(){
+  this.lists.push(this.newlist)
+  }
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
