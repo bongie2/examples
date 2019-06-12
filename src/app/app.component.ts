b3e34c980e43700b62658145c602c3660ea3df05
Bongie <makgomo05@gamil.com>
2019-06-12 12:51:54 +0200

    list Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { fromStringWithSourceMap } from 'source-list-map';
 
 @Component({
   selector: 'app-root',
@@ -26,10 +25,59 @@ export class AppComponent {
   //let index=this.lists.indexOf(list)
   //this.lists.splice(index,1)
 //}
-lists=["Animals","Fruits","Vegetables"];
-newlist:string='';
-AddNew(){
-  this.lists.push(this.newlist)
-  }
+//lists=["Animals","Fruits","Vegetables"];
+//newlist:string='';
+//AddNew(){
+  //this.lists.push(this.newlist)
+   //}
+   //RemoveList(list){
+    //let index=this.lists.indexOf(list)
+    //this.lists.splice(index,1)
+   //}
+
+ //editlist(list)
+ //{
+//let index=this.lists.indexOf(list)
+
+//let newname =prompt("edit your list",list)
+ // if(newname!=null)
+//{
+  //list[index]=newname
+   
+//}
+//}
+tasks=[];
+empty=true
+newTask : string = "";
+newPriority : string = "";
+
+addNew(){
+this.tasks.push({taskName:this.newTask ,priority:this.newPriority});
+
+this.newTask="";
+this.newPriority="";
+if(this.tasks.length>0){
+  this.empty=false
 }
+}
+
+deleteTask(task){
+  let index=this.tasks.indexOf(task)
+  this.tasks.splice(index,1)
+  if(this.tasks.length<=0){
+    this.empty=true}
+}
+editTask(task){
+  
+  let newTaskName = prompt("edit your new taskName");
+ let newPrio= prompt("edit your new priority");
+ let index=this.tasks.indexOf(task);
+  if((newTaskName!=null) && (newPrio!=null))  {
+    this.tasks[index].taskName= newTaskName;
+    this.tasks[index].priority=  newPrio;
+  }  
+}
+}
+   
+
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule,MatInputModule,MatButtonModule,MatFormFieldModule,MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -11,9 +13,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+        FormsModule,
+        BrowserAnimationsModule,
+        MatCardModule, 
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
